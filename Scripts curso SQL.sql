afb67feca554b2a7dbf8d07d759c402c836af8e4
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2023-04-23 22:44:40 -0300

    Update Scripts curso SQL.sql

--- Scripts curso SQL.sql
+++ Scripts curso SQL.sql
@@ -1,4 +1,5 @@
 use curso_sql;
+*.rb linguist-language=SQL
 
 insert into funcionarios values (1,'Fernando', 1400, 'TI');
 insert into funcionarios (nome,salario, departamento)values ('Guilherme', 2500, 'Departamento Pessoal');
